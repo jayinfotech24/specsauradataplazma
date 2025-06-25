afd8418089f37a20c7810f2b5c33c950bc53c2d8
Jay Mevada <jayinfotech24@gmail.com>
2025-06-26 00:32:31 +0530

    added default null values for cart model

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -14,16 +14,19 @@ const cartSchema = new mongoose.Schema(
         lensType: { 
             type: mongoose.Schema.Types.ObjectId,
             ref: MODEL_NAME.LENS_TYPE,
+            default: null
          },
         numberOfItems: { type: Number, required: true, min: 1 },
         lensCoating: { 
             type: mongoose.Schema.Types.ObjectId,
             ref: MODEL_NAME.COATING,
-         }, 
+            default: null 
+        }, 
         lensMaterial: { type: String, default: null },
         prescriptionID: {
             type: mongoose.Schema.Types.ObjectId,
             ref: MODEL_NAME.PRESCRIPTION,
+            default: null
         },
         isDelete: { type: Boolean, default: false},
         isAllDataAdded: { type: Boolean, default: false}
