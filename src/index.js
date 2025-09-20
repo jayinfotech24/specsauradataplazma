cc82e29ae44bdaf74eb9c9df7258a036e435bddb
Jay Mevada <jayinfotech24@gmail.com>
2025-09-20 18:10:44 +0530

    cors changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: true,       // reflect the request origin
+    credentials: true   // allow cookies, Authorization headers, etc.
+  }));
+app.options("*", cors());
 
 // Routes
 app.use("/api/", globalRoute);
